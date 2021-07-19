a257d8c9e5fb6890ae998ecb4de3472d60539dfd
khabiir <khabiir2018@gmail.com>
2021-07-19 12:37:11 +0300

    Formatted strings is added

---
+++ Format-Strings.py
@@ -0,0 +1,13 @@
+# Formmatted Strings
+
+first_name = "Bashir"
+last_name = "Abdullahi"
+# to combine both string values in the variable
+
+# First Approach
+full_name = first_name + " " + last_name
+
+# Second Approach
+
+full_name = f" {first_name} {last_name}"
+print(full_name)
