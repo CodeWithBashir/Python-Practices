eb7ba76ed1ba8c2abba886bfe552d08c8cb3405e
khabiir <khabiir2018@gmail.com>
2021-07-19 15:50:32 +0300

    Short Circuit Evaluation is added

--- __pycache__/Part Three/Logical_Operator.py
+++ __pycache__/Part Three/Logical_Operator.py
@@ -25,3 +25,10 @@ if not student:
     print("Eligible")
 else:
     print("Not Eligible")
+
+# Short Circuit Evaluation
+high_income = True
+good_credit = True
+student = True
+if high_income or good_credit and not student:
+    print("Eligible")
