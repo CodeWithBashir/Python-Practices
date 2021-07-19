3263f7bf8ab6df9d9a253d5398d20bc56aeee757
khabiir <khabiir2018@gmail.com>
2021-07-19 14:17:20 +0300

    Part Two Quiz is added

---
+++ Quiz.py
@@ -0,0 +1,22 @@
+#1  What are the primitive types in python
+#1 Answer: 1. Numbers 2. Strings 3. Booleans and
+#1 Numbers can be Integer, Float or Complex Numbers
+
+#2 What is the output of this code
+#  fruit = "Apple"
+# print(fruits[1])
+
+#2 Answer is the first letter P and PPL
+fruit = "Apple"
+print(fruit[1])
+
+print(fruit[1:-1])
+
+#3 What is the result of this expression 10 % 3
+#3 The answer is 1
+print(10%3)
+
+#4 What is the ouput of this expression print(bool("False"))
+#4 Answer is True
+print(bool("False"))
+Output true
