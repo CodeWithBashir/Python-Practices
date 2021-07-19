c230aba2d4bab8f6bbe2d94c5dfa18bf51f945c7
khabiir <khabiir2018@gmail.com>
2021-07-19 13:38:48 +0300

    Numbers in python is added

---
+++ numbers.py
@@ -0,0 +1,18 @@
+# There three types of number
+# 1. Integer 2. Float 3.Complex
+
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+print(10 // 3)
+print(10 % 3)
+print(10 ** 3)
+#  if you want to make te output 
+# of this  print(10 / 3) integer 
+# use this double back slash //
+
+x = 10 
+# argumented assignment
+x += 3
+print(x)
