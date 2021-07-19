807932ca74889f8e9bee838aa04f10333038aced
khabiir <khabiir2018@gmail.com>
2021-07-19 16:05:42 +0300

    Chaining Comparison Operator is added

---
+++ __pycache__/Part Three/Chaining-Comparison-Operators.py
@@ -0,0 +1,11 @@
+# Age should be between 18 and 65
+
+# Normal statement
+age = 22
+if age >= 18 and age < 65:
+    print("Eligible")
+
+
+# Chaining Comparison Operator
+if 18 <= age < 65:
+    print("Eligible")
