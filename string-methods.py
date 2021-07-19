5c73420439ffa26db38320a11047fa4fcc0791be
khabiir <khabiir2018@gmail.com>
2021-07-19 13:26:51 +0300

    String methods is added

---
+++ string-methods.py
@@ -0,0 +1,24 @@
+
+# Strings Methods
+
+course = "   Python Programming"
+print(course.upper())
+print(course.lower())
+print(course.title())
+
+# Removing white spaces
+print(course.rstrip())
+
+# Finding indexes
+print(course.find("ram"))
+
+# Replace
+print(course.replace("P", "J"))
+print(course.replace("mm", "gg"))
+
+print("Pro" in course)
+print("Jro" in course)
+print("Java" not in course)
+
+# There are alot of these
+#  which we didn't cover in here
