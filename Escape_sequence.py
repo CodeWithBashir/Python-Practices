4e109add4113cf24431b49d384c4510e0c056e95
khabiir <khabiir2018@gmail.com>
2021-07-19 12:26:23 +0300

    Escape Sequence and Character is added

---
+++ Escape_sequence.py
@@ -0,0 +1,12 @@
+# Escape Sequence
+# we can use these characters
+# \"
+# \'
+# \\
+# \n
+
+course = "Python \"Programming"
+course = "Python \'Programming"
+course = "Python \\Programming"
+course = "Python \nProgramming"
+print(course)
