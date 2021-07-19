36f069cc61fea78d41a080a94e313eac2208f516
khabiir <khabiir2018@gmail.com>
2021-07-19 15:25:38 +0300

    Ternary Operatory is added

---
+++ __pycache__/Part Three/Ternary-Operator.py
@@ -0,0 +1,12 @@
+#  Normal Condition Statement code
+
+age = 20
+if age >= 18:
+    print("Eligible")
+else:
+    print("Not Eligible")
+#  Ternary Condition Statement code
+
+age = 22
+message = "Eligible" if age >= 18 else "Not Eligible"
+print(message)
