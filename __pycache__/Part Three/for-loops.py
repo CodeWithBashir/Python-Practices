815ddda0f1e9894966d4cb2589e2914e1cc5b61f
khabiir <khabiir2018@gmail.com>
2021-07-20 10:13:34 +0300

    For loops is added

---
+++ __pycache__/Part Three/for-loops.py
@@ -0,0 +1,11 @@
+
+# # Statement One
+# for number in range(3):
+#     print("Attempt", number + 1, (number + 1) * ".")
+
+# # Statement Two
+# for number in range(1, 4):
+#     print("Attempt", number + 1, number * ".")
+
+for number in range(1, 10, 2):
+    print("Attempt", number, number * ".")
