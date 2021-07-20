988595de21781c6302431129167596cc0755ed94
khabiir <khabiir2018@gmail.com>
2021-07-20 23:36:28 +0300

    All part four code are added

---
+++ __pycache__/PartFour/Types-of-functions.py
@@ -0,0 +1,13 @@
+
+
+def get_greeting(name):
+    return f"Hello {name}"
+
+
+message = get_greeting("Bashiir")
+print(message)
+
+
+# There two types of Function exist in python
+# 1 Perform a Task
+# 2 Return a Value
---
+++ __pycache__/PartFour/xargs.py
@@ -0,0 +1,18 @@
+# def multiply(*numbers):
+#     total = 1
+#     for number in numbers:
+#         total *= number
+#     return total
+
+
+# print(multiply(2, 3, 4, 5))
+
+
+def save_user(**user):
+    print(user)
+
+
+save_user(id=1, name="Basraan", age=22)
+
+# when we use two astras means we can
+# give more values to the arguments
