627e26051efd93fd261ddcf1901acb099dc04980
khabiir <khabiir2018@gmail.com>
2021-07-20 21:36:38 +0300

    Arguments is added

---
+++ __pycache__/PartFour/arguments.py
@@ -0,0 +1,11 @@
+# Arguments in python
+
+def greeting(first_name, last_name):
+    print(f"Hello {first_name} {last_name}")
+    print("Welcome aboard")
+
+
+greeting("Bashiir", "Abdullaahi")
+greeting("Basraan", "Omer")
+
+# End of Arguments in python
